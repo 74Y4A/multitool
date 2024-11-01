2281b9dccf5a943a1c166ca37ac9c96a328bdb60
74Y4A <ahmad.rachid44813@gmail.com>
2024-11-01 06:03:11 +0300

    Add files via upload

---
+++ main.bat
@@ -0,0 +1,42 @@
+@echo off
+title MultiTool - by 7moodi
+chcp 65001 >nul
+:start
+call :banner
+cd files
+
+:menu
+for /f %%A in ('"prompt $H &echo on &for %%B in (1) do rem"') do set BS=%%a
+echo.
+echo.
+echo         ╔═(1) bruteforce
+echo         ║
+echo         ╠══(2) ipscan
+echo         ║
+echo         ╠═══(3) iptool
+echo         ║
+echo         ╚╦═══(4) sessionsploit
+echo          ║
+set /p input=.%BS%      ╚═══════^>
+if /I %input% EQU 1 start bruteforce.bat
+if /I %input% EQU 2 start ipscan.exe
+if /I %input% EQU 2 start iptool.bat
+if /I %input% EQU 2 start sessionsploit.exe
+cls
+goto start
+echo
+:banner
+echo.
+echo.
+echo                  ██░ ██  ▄▄▄       █    ██  ███▄    █ ▄▄▄█████▓▓█████ ▓█████▄ 
+echo                 ▓██░ ██▒▒████▄     ██  ▓██▒ ██ ▀█   █ ▓  ██▒ ▓▒▓█   ▀ ▒██▀ ██▌
+echo                 ▒██▀▀██░▒██  ▀█▄  ▓██  ▒██░▓██  ▀█ ██▒▒ ▓██░ ▒░▒███   ░██   █▌
+echo                 ░▓█ ░██ ░██▄▄▄▄██ ▓▓█  ░██░▓██▒  ▐▌██▒░ ▓██▓ ░ ▒▓█  ▄ ░▓█▄   ▌
+echo                 ░▓█▒░██▓ ▓█   ▓██▒▒▒█████▓ ▒██░   ▓██░  ▒██▒ ░ ░▒████▒░▒████▓ 
+echo                  ▒ ░░▒░▒ ▒▒   ▓▒█░░▒▓▒ ▒ ▒ ░ ▒░   ▒ ▒   ▒ ░░   ░░ ▒░ ░ ▒▒▓  ▒ 
+echo                   ▒ ░▒░ ░  ▒   ▒▒ ░░░▒░ ░ ░ ░ ░░   ░ ▒░    ░     ░ ░  ░ ░ ▒  ▒ 
+echo                  ░  ░░ ░  ░   ▒    ░░░ ░ ░    ░   ░ ░   ░         ░    ░ ░  ░ 
+echo                  ░  ░  ░      ░  ░   ░              ░             ░  ░   ░    
+echo                                                                        ░      
+echo.
+echo.
